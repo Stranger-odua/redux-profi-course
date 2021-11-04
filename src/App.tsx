d3ea4061f45d7a72e53edee2ff9905ffe7d1ee5e
Stranger <rooderuler@gmail.com>
2021-11-04 19:10:08 +0200

    Создаем и обрабатываем асинхронные запросы на сервер с RTK Querry

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import './App.css';
 import {useAppDispatch, useAppSelector} from "./hooks/redux";
 import {userSlice} from "./store/reducers/UserSlice";
 import {fetchUsers} from "./store/reducers/ActionCreators";
+import PostContainer from "./components/PostContainer";
 
 
 function App() {
-    const dispatch = useAppDispatch()
-    const {users, isLoading, error} = useAppSelector(state => state.userReducer)
-
-    useEffect(() => {
-        dispatch(fetchUsers())
-    }, [])
+    // const dispatch = useAppDispatch()
+    // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+    //
+    // useEffect(() => {
+    //     dispatch(fetchUsers())
+    // }, [])
 
     return (
         <div className="App">
-            {isLoading && <h1>Идет загрузка...</h1>}
-            {error && <h1>{error}</h1>}
-            {JSON.stringify(users, null, 2)}
+            {/*{isLoading && <h1>Идет загрузка...</h1>}*/}
+            {/*{error && <h1>{error}</h1>}*/}
+            {/*{JSON.stringify(users, null, 2)}*/}
+
+            <PostContainer/>
         </div>
     );
 }
